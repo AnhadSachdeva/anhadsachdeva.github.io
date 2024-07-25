a09b4faee206d14ce955c35a610df356a0c357d3
Anhad Sachdeva <lappy@Anhads-MacBook-Pro.local>
2024-07-25 18:07:21 +0930

    made the buttons responsive

---
+++ root/index.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('find_out_more');
+
+    button.addEventListener('click', function() {
+        const projects = document.getElementById('projects_layout');
+        if(projects){
+            projects.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('projects');
+
+    button.addEventListener('click', function() {
+        const projects = document.getElementById('projects_layout');
+        if(projects){
+            projects.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+    });
+});
